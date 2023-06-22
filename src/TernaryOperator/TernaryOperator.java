69ba253953e4f31ee66f18f7ab5830a7e5a22f3e
Misho00 <mihail_ankov21@abv.bg>
2023-06-22 13:12:33 +0300

    Section 4: Intellij Basics - 38. Ternary Operator

---
+++ src/TernaryOperator/TernaryOperator.java
@@ -0,0 +1,21 @@
+package TernaryOperator;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = (makeOfCar == "Volkswagen") ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+        int ageOfClient = 20;
+
+        String ageText = (ageOfClient >= 18) ? "Over Eighteen" : "Still a kid";
+        System.out.println("Our client is " + ageText);
+    }
+}
