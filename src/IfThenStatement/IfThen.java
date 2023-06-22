5af09e62b3f08142eaf40118ce258d1a44ecbf37
Misho00 <mihail_ankov21@abv.bg>
2023-06-22 12:32:43 +0300

    Java Programming Masterclass updated to Java 17 initial commit

---
+++ src/IfThenStatement/IfThen.java
@@ -0,0 +1,13 @@
+package IfThenStatement;
+
+public class IfThen {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I'm afraid of aliens!");
+        }
+    }
+}
