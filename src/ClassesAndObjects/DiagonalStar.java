cf1665c700cc51392f86eff4c6fad3c1101ef189
Misho00 <mihail_ankov21@abv.bg>
2023-06-26 16:05:01 +0300

    Section 6: Control Flow - 70-72. Reading Input with Scanner.

---
+++ src/ClassesAndObjects/DiagonalStar.java
@@ -0,0 +1,38 @@
+package ClassesAndObjects;
+
+public class DiagonalStar {
+
+    public static void main(String[] args) {
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar (int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int row = 0; row < number; row++) {
+            for (int column = 0; column < number; column++) {
+                if
+                (
+                    row == 0 ||
+                    column == 0 ||
+                    row == number - 1 ||
+                    column == number - 1 ||
+                    row == column ||
+                    column == number - 1 - row
+
+                )
+                {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
