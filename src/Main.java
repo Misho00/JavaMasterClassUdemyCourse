bb808abfb1e6a9d977cb489a3222b3d89c6e98cc
Misho00 <mihail_ankov21@abv.bg>
2023-06-28 11:58:21 +0300

    Section 7: OOP - 87 Coding exercise - Complex operations

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
-import OOP.Calculator;
-import OOP.Carpet;
-import OOP.Floor;
+import OOP.ComplexNumber;
 
 public class Main {
     public static void main(String[] args) {
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.0);
-        Calculator calculator = new Calculator(floor, carpet);
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
 
-        System.out.println("total= " + calculator.getTotalCost());
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
 
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
 
-        System.out.println("total= " + calculator.getTotalCost());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
     }
 }
