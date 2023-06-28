d6118c005242bf12547cb32380290e9ee6dce75a
Misho00 <mihail_ankov21@abv.bg>
2023-06-28 11:23:46 +0300

    Section 7: OOP - 86 & 87 POJO and Record

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,38 @@
-import OOP.Customer;
+import OOP.LPAStudent;
+import OOP.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        System.out.println(customer.getName());
-        System.out.println(customer.getCreditLimit());
-        System.out.println(customer.getEmail());
 
-        System.out.println("----------------");
+        for (int i = 1; i <= 5; i++) {
+            LPAStudent s = new LPAStudent("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Marry";
+                        case 2 -> "John";
+                        case 3 -> "Kim";
+                        case 4 -> "Tom";
+                        case 5 -> "Jim";
+                        default -> "Anonymous";
+                    },
+                    "05-11-1995",
+                    "Java Master Class");
 
-        Customer customer2 = new Customer("John", 250, "john@john.com");
-        System.out.println(customer2.getName());
-        System.out.println(customer2.getCreditLimit());
-        System.out.println(customer2.getEmail());
+            System.out.println(s);
+        }
+        System.out.println("-----------------");
 
-        System.out.println("----------------");
+        Student pojoStudent = new Student("S923006", "Ann", "05-11-1995", "Java Master Class");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05-11-1995", "Java Master Class");
 
-        Customer customer3 = new Customer("Kim", "kim@kim.com");
-        System.out.println(customer3.getName());
-        System.out.println(customer3.getCreditLimit());
-        System.out.println(customer3.getEmail());
+        System.out.println(pojoStudent);
+        System.out.println("---------------");
+        System.out.println(recordStudent);
+        System.out.println("---------------");
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
---
+++ src/OOP/LPAStudent.java
@@ -0,0 +1,6 @@
+package OOP;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+
+
+}
