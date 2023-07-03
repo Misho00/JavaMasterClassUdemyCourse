16ff20986604813b18ad466bad6486d1f420075b
Misho00 <mihail_ankov21@abv.bg>
2023-07-03 16:53:10 +0300

    Section 8: OOP Part 2 Polymorphism - 111 & 112 Polymorphism Challenge part 1 & 2

---
+++ src/OOPPartTwo/PolymorphismChallenge/Car.java
@@ -0,0 +1,25 @@
+package OOPPartTwo.PolymorphismChallenge;
+
+public class Car {
+
+    private String description;
+
+    public Car(String description) {
+        this.description = description;
+    }
+
+    public void startEngine() {
+        System.out.println("Car -> startEngine");
+    }
+
+    protected void runEngine() {
+        System.out.println("Car -> runEngine");
+    }
+
+    public void drive() {
+        System.out.println("Car -> driving, type is " + getClass().getSimpleName());
+        runEngine();
+    }
+
+
+}
