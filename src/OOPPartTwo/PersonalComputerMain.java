97a69ac83d85e73981d2aca7571465a831b5cb96
Misho00 <mihail_ankov21@abv.bg>
2023-07-03 10:35:01 +0300

    Section 8: OOP Part 2 Polymorphism - 101 & 102 Composition Part 1 & 2

---
+++ src/OOPPartTwo/PersonalComputerMain.java
@@ -0,0 +1,17 @@
+package OOPPartTwo;
+
+public class PersonalComputerMain {
+    public static void main(String[] args) {
+        ComputerCase theCase = new ComputerCase("2208", "Dell", "240");
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, "2540 x 1440");
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+        PersonalComputer thePC = new PersonalComputer("2208", "Dell", theMonitor, theMotherboard, theCase);
+
+        /*thePC.getMonitor().drawPixelAt(10, 10, "red");
+        thePC.getMotherboard().loadProgram("Window OS");
+        thePC.getComputerCase().pressPowerButton();*/
+
+        thePC.powerUp();
+
+    }
+}
