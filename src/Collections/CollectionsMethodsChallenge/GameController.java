53307975236874cddd48c1a12c12e8777e2aa1b1
Misho00 <mihail_ankov21@abv.bg>
2023-07-26 12:04:06 +0300

    Section 15: Collections - 202-203 Collections Methods Challenge

---
+++ src/Collections/CollectionsMethodsChallenge/GameController.java
@@ -0,0 +1,8 @@
+package Collections.CollectionsMethodsChallenge;
+
+public class GameController {
+    public static void main(String[] args) {
+        PokerGame fiveCardDraw = new PokerGame(4, 5);
+        fiveCardDraw.startPlay();
+    }
+}
